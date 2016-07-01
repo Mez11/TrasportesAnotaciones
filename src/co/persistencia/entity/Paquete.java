638d35d6c214920110d1bab758c49d8b1b4cf405
Misael Alanis <misaelarturo@gmail.com>
2016-07-01 00:27:51 -0500

    Corregidos nombres de la clase Paquete.java

--- src/co/persistencia/entity/Paquete.java
+++ src/co/persistencia/entity/Paquete.java
@@ -1,34 +1,16 @@
 package co.persistencia.entity;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 //import org.hibernate.annotations.Table;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.IndexColumn;
 
 @Entity
 @Table (name ="paquete")
 public class Paquete {
-	
-	public Paquete( String codigo, String descricion, String destinatario, String direccion_destinatario,
-			Camionero id_camionero, Provincia provincia) {
-		super();
-		
-		this.codigo = codigo;
-		this.descricion = descricion;
-		this.destinatario = destinatario;
-		this.direccion_destinatario = direccion_destinatario;
-		this.id_camionero = id_camionero;
-		this.provincia = provincia;
-	}
 	@Id
 	@Column(name="id")
 	private int id;
@@ -43,23 +25,27 @@ public class Paquete {
 	private String destinatario;
 	
 	@Column(name ="direccion_destinatario")
-	private String direccion_destinatario;
+	private String direccionDestinatario;
 	
 	@ManyToOne
 	@JoinColumn(name = "id_camionero")
     @Column(name ="id_camionero")
-    private Camionero id_camionero;
+    private Camionero camionero;
   
-	  public Camionero getId_camionero() {
-		return id_camionero;
-	}
-	public void setId_camionero(Camionero id_camionero) {
-		this.id_camionero = id_camionero;
-	}
 	//Relaciones
 	@ManyToOne
 	@JoinColumn(name = "id_provincia")
 	private Provincia provincia;
+	
+	public Paquete( String codigo, String descricion, String destinatario, String direccion_destinatario,
+			Camionero camionero, Provincia provincia) {
+		this.codigo = codigo;
+		this.descricion = descricion;
+		this.destinatario = destinatario;
+		this.direccionDestinatario = direccion_destinatario;
+		this.camionero = camionero;
+		this.provincia = provincia;
+	}
 	
 	public Provincia getProvincia() {
 		return provincia;
