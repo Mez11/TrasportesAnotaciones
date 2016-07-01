d49ff2b606196e0cde3a1589094ff546ff24d9a8
Joscelyn Meztli <meztli.swim@gmail.com>
2016-07-01 00:45:50 -0500

    Paquete se convirtio a Bean

--- src/co/persistencia/entity/Paquete.java
+++ src/co/persistencia/entity/Paquete.java
@@ -1,5 +1,7 @@
 package co.persistencia.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,7 +12,12 @@ import javax.persistence.Table;
 
 @Entity
 @Table (name ="paquete")
-public class Paquete {
+public class Paquete implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@Column(name="id")
 	private int id;
@@ -26,6 +33,36 @@ public class Paquete {
 	
 	@Column(name ="direccion_destinatario")
 	private String direccionDestinatario;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "id_camionero")
+    @Column(name ="id_camionero")
+    private Camionero camionero;
+  
+	//Relaciones
+	@ManyToOne
+	@JoinColumn(name = "id_provincia")
+	private Provincia provincia;
+	
+	public Paquete( String codigo, String descricion, String destinatario, String direccion_destinatario,
+			Camionero camionero, Provincia provincia) {
+		this.codigo = codigo;
+		this.descricion = descricion;
+		this.destinatario = destinatario;
+		this.direccionDestinatario = direccion_destinatario;
+		this.camionero = camionero;
+		this.provincia = provincia;
+		
+	}
+	
+	public Provincia getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(Provincia provincia) {
+		this.provincia = provincia;
+	}
+	
 	
 	public int getId() {
 		return id;
@@ -74,33 +111,7 @@ public class Paquete {
 	public void setCamionero(Camionero camionero) {
 		this.camionero = camionero;
 	}
-	@ManyToOne
-	@JoinColumn(name = "id_camionero")
-    @Column(name ="id_camionero")
-    private Camionero camionero;
-  
-	//Relaciones
-	@ManyToOne
-	@JoinColumn(name = "id_provincia")
-	private Provincia provincia;
 	
-	public Paquete( String codigo, String descricion, String destinatario, String direccion_destinatario,
-			Camionero camionero, Provincia provincia) {
-		this.codigo = codigo;
-		this.descricion = descricion;
-		this.destinatario = destinatario;
-		this.direccionDestinatario = direccion_destinatario;
-		this.camionero = camionero;
-		this.provincia = provincia;
-		
-	}
-	
-	public Provincia getProvincia() {
-		return provincia;
-	}
-	public void setProvincia(Provincia provincia) {
-		this.provincia = provincia;
-	}
 }
 
 	
